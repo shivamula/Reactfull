0b546d5be2b772079fa98c29a9df34d240117418
shivakumar <shivakumar1342@gmail.com>
2022-02-23 16:30:23 +0530

    added file

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <h1>Hotstar</h1>
+      <nav>
+        <ul>
+          <li>pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
